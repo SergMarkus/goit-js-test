f9df246cbfa958da83f61d6625bff3315fc1d930
Sergii Morozov <sergmarkus@gmail.com>
2024-07-21 00:19:58 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,57 @@
-function makeTransaction(quantity, pricePerDroid) {
-    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
-};
+// Якщо змінна a дорівнює 10, виведіть 'Вірно', інакше виведіть 'Невірно'.
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+// function checkNumber(a) {
+//     if (a === 10) {
+//         return "Вірно";
+//     } else {
+//         return "Невірно"
+//     }
+// }
+// console.log(checkNumber(10));
+
+// function checkNumber(a) {
+//     return a === 10 ? "Вірно" : "Невірно"; //тернарний
+// }
+
+// console.log(checkNumber(10));
+
+
+
+// Змінна num може набувати 4 значення: 1, 2, 3 або 4. Якщо вона має
+// значення '1', то у змінну result запишемо 'зима', якщо має значення
+// '2' - 'весна' і так далі.
+
+// function checkSeason(num) {
+//     // let season = "";
+//     switch (num) {
+//         case 1:
+//             return "зима";
+//         case 2:
+//             return "весна";
+//         case 3:
+//             return "літо";
+//         case 4:
+//             return "осінь";
+//         default:
+//             // season = "No season! Chose numbers 1-4";
+//             return "No season! Chose numbers 1-4"
+//     }
+// }
+// console.log(checkSeason(5));
+
+
+// Напишіть програму, яка отримає від користувача
+//число (кількість хвилин) і виведе у консоль
+//рядок у форматі годин і хвилин
+//70 === 01:10
+
+function formatTime(totalMinutes) {
+    
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+    const formattedHours = hours < 10 ? '0' + hours : hours;
+    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+    return `${formattedHours}:${formattedMinutes}`;
+}   
+console.log(formatTime(70));
+
